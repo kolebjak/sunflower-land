8766839b0fd8ac934d4d9fcf85ab1b064cb5dbed
Spencer <spencer.dezart.smith@gmail.com>
2022-03-13 11:45:05 +1100

    Fix game machine instance change

--- src/features/game/GameProvider.tsx
+++ src/features/game/GameProvider.tsx
@@ -1,7 +1,7 @@
 /**
  * A wrapper that provides game state and dispatches events
  */
-import { useState, useCallback } from "react";
+import { useState, useCallback, useEffect } from "react";
 import { useActor, useInterpret } from "@xstate/react";
 import React, { useContext } from "react";
 
@@ -22,15 +22,16 @@ export const Context = React.createContext<GameContext>({} as GameContext);
 export const GameProvider: React.FC = ({ children }) => {
   const { authService } = useContext(Auth.Context);
   const [authState] = useActor(authService);
-
-  // TODO - Typescript error
-  const gameService = useInterpret(
+  const [gameMachine] = useState(
     startGame({
       ...authState.context,
       // If the last event was a create farm, walk them through the tutorial
       isNoob: authState.history?.event.type === "CREATE_FARM",
     }) as any
-  ) as MachineInterpreter;
+  );
+
+  // TODO - Typescript error
+  const gameService = useInterpret(gameMachine) as MachineInterpreter;
   const [shortcuts, setShortcuts] = useState<InventoryItemName[]>(
     getShortcuts()
   );
